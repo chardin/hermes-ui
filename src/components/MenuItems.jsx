fb13f5c62eacb431c14338a321540ef5ff3e3f77
Chuck Hardin <chardin@gmail.com>
2026-07-29 15:14:17 -0600

    Correct source code spacing - closes #2

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -2,31 +2,31 @@ import { useState } from 'react';
 import Dropdown from './Dropdown';
 
 const MenuItems = ({ items, depthLevel }) => {
-  const [dropdown, setDropdown] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
-  return (
-    <li 
-      className="menu-items"
-      onMouseEnter={() => setDropdown(true)}
-      onMouseLeave={() => setDropdown(false)}
-    >
-      {items.submenu ? (
-        <>
-          <button type="button" aria-haspopup="menu" aria-expanded={dropdown ? "true" : "false"}>
-            {items.title}{' '}
-            {depthLevel > 0 ? <span>&raquo;</span> : <span className="arrow" />}
-          </button>
-          <Dropdown 
-            submenus={items.submenu} 
-            dropdown={dropdown} 
-            depthLevel={depthLevel} 
-          />
-        </>
-      ) : (
-          <a href={items.url} onClick={items.onClick}> {items.title} </a>
-      )}
-    </li>
-  );
+    return (
+	<li 
+	    className="menu-items"
+	    onMouseEnter={() => setDropdown(true)}
+	    onMouseLeave={() => setDropdown(false)}
+	>
+	    {items.submenu ? (
+		<>
+		    <button type="button" aria-haspopup="menu" aria-expanded={dropdown ? "true" : "false"}>
+			{items.title}{' '}
+			{depthLevel > 0 ? <span>&raquo;</span> : <span className="arrow" />}
+		    </button>
+		    <Dropdown 
+			submenus={items.submenu} 
+			dropdown={dropdown} 
+			depthLevel={depthLevel} 
+		    />
+		</>
+	    ) : (
+		<a href={items.url} onClick={items.onClick}> {items.title} </a>
+	    )}
+	</li>
+    );
 };
 
 export default MenuItems;
