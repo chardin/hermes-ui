4d4670c0a5d920737a577cbab15ab0dc23b4c91e
Chuck Hardin <chardin@gmail.com>
2026-08-02 15:40:41 -0600

    Change password page - closes #17

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,7 +12,7 @@ function Profile(props) {
     const [blobUrl, setBlobUrl] = useState(null);
     const [wakeLock, setWakeLock] = useState(null);
     const [menuData, setMenuData] = useState(null);
-
+    
     const expandToSecondLevel = (level) => level < 2;
 
     const logMeOut = async() => {
@@ -31,6 +31,87 @@ function Profile(props) {
 	})
     }
 
+    const DisplayUser = async(user_data) => {
+	setWidget(
+	    <div>
+		<dl>
+		    <dt>Username</dt>
+		    <dd>{user_data.username}</dd>
+		</dl>
+		<dl>
+		    <dt>Full Name</dt>
+		    <dd>{user_data.full_name}</dd>		
+		</dl>
+	    </div>
+	);
+    }
+
+    const ChangePassForm = async() => {
+	setWidget(
+	    <div>
+		<form id="ChangePass" onSubmit={ChangePass}>
+		    <label>Current Password:</label>
+		    <input
+			type="password"
+			id="current_password"
+			name="current_password"
+			required></input>
+		    <br />
+		    <label>New Password:</label>
+		    <input
+			type="password"
+			id="new_password"
+			name="new_password"
+			required></input>
+		    <br />
+		    <label>Confirm New Password:</label>
+		    <input
+			type="password"
+			id="confirm_password"
+			name="confirm_password"
+			required></input>
+		    <br />
+		    <button type="submit">Update Password</button>
+		</form>
+	    </div>
+	);
+    }
+
+    const ChangePass = async() => {
+	event.preventDefault();
+	const form = event.target;
+	const formData = new FormData(form);
+
+	const formValues = Object.fromEntries(formData.entries());
+	
+    	fetch('/api/change_password', {
+	    method: 'POST',	    
+	    headers: {
+		'Authorization': 'Bearer ' + props.token,
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+	    },
+	    body: JSON.stringify(formValues),
+	})
+	.then((response) => {
+	    if (!response.ok) {
+		throw new Error("HTTP error! Status: ${response.status}");
+	    }
+	    return response.json();
+	})
+	.then((json) => {
+	    if (json.success) {
+		setWidget(
+		    <p>Password changed!</p>
+		);
+	    }
+	    else {
+		setWidget(
+		    <p>Error: {json.error}</p>
+		);
+	    }
+	});
+    }
     
     const requestWakeLock = async () => {
 	if (!('wakeLock' in navigator)) {
@@ -223,11 +304,11 @@ function Profile(props) {
 		img: heroImage
 	    },
 	    {
-		title: 'Profile',
+		title: 'User',
 		url: '#',
 		submenu: [
-		    { text: 'Username: ' + data.user.username },
-		    { text: 'Full name: ' + data.user.full_name }
+		    { title: 'Info', url: '#', onClick: () => DisplayUser(data.user) },
+		    { title: 'Change Password', url: '#', onClick: () => ChangePassForm() }
 		]
 	    },
 	    {
@@ -244,12 +325,6 @@ function Profile(props) {
 		url: '#',
 		onClick: () => GetHistoryList(0, 0)
 	    },
-	    {
-		title: 'Logout',
-		url: '#',
-		onClick: () => logMeOut()
-	    },
-	    
 	]);
     }
     
