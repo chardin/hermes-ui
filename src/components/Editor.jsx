fb7026c5877b4ca9b613823ffc404081b8dd0eba
Chuck Hardin <chardin@gmail.com>
2026-08-18 15:29:09 -0600

    Begining to flesh out editing a routine

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,10 @@
 export function ShowRoutines(setWidget, props, routines) {
-    console.log(routines);
     routines.sort((a, b) => a.name.localeCompare(b.name));
     setWidget(
 	<div className="flexible-grid-container">
 	    {routines.map((routine) => (
 		<div key={routine.routine_id} className="grid-item">
-		    <a href='#' key={'link'+routine.routine_id} onClick={() => EditRoutineForm(setWidget, routine)}>{routine.name}</a>
+		    <a href='#' key={'link'+routine.routine_id} onClick={() => EditRoutineForm(setWidget, props, routine)}>{routine.name}</a>
 		</div>
 	    ))}
 	</div>
@@ -25,7 +24,7 @@ export function ShowExercises(setWidget, props, exercises) {
     );
 }
 
-export function EditRoutineForm(setWidget, routine) {
+export function EditRoutineForm(setWidget, props, routine) {
     setWidget(
 	<div className="form-group">
 	    <form id="EditRoutine" onSubmit={(e) => SaveRoutine(e, setWidget, props)}>
@@ -45,27 +44,69 @@ export function EditRoutineForm(setWidget, routine) {
 		    required
 		/>
 		<br />
+		<h3>Exercises:</h3>
+		<table>
+		    <thead>
+			<tr>
+			    <th>Name</th>
+			    <th>Order</th>
+			    <th>Sets</th>
+			    <th>Reps</th>
+			    <th>Paused?</th>
+			</tr>
+		    </thead>
+		    <tbody>
 		{routine.exercises.map((exercise) => (
-		    <>
-			<label htmlFor='exercise_name'>{exercise.name}</label>
-			<input
-			    id={exercise.exercise_id}
-			    key={exercise.exercise_id}
-			    type='text'
-			    name='order'
-			    defaultValue={exercise.order}
-			/>
-			<br />
-		    </>
+		    <tr key={'exercise-'+exercise.exercise_id}>
+			    <td>
+				<input
+				    name={'name-'+exercise.exercise_id}
+				    id={'name-'+exercise.exercise_id}
+				    key={'name-'+exercise.exercise_id}
+				    type='text'
+				    defaultValue={exercise.name}
+				/>
+			    </td>
+			    {IntegerWidget('order', exercise.exercise_id, exercise.order)}
+			    {IntegerWidget('num_sets', exercise.exercise_id, exercise.num_sets)}
+			    {IntegerWidget('num_reps', exercise.exercise_id, exercise.num_reps)}
+			    <td>
+				<input
+				    name={'is_paused-' + exercise.exercise_id}
+				    id={'is_paused-' + exercise.exercise_id}
+				    key={'is_paused-' + exercise.exercise_id}
+				    type='checkbox'
+				    defaultChecked={exercise.is_paused}				    
+				/>
+			    </td>
+			</tr>
 		    
 		))}
-		
-		<br />
+		    </tbody>
+		</table>			
 		<button type="submit">Save Routine</button>
 	    </form>
 	</div>
     );
 }
+
+function IntegerWidget(baseName, id, defaultValue) {
+    return (
+	<td>
+	    <button>-</button>
+	    <input
+		name={baseName + '-' + id}
+		id={baseName + '-' + id}
+		key={baseName + '-' + id}
+		size="2"
+		type='text'
+		defaultValue={defaultValue}
+		required
+	    />
+	    <button>+</button>
+	</td>
+    );
+}	
 
 export function SaveRoutine(event, setWidget, props) {
     event.preventDefault();
